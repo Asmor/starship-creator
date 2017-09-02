5564fee66dfe163a48ea836f7cf12bb16241b186
asmor <itoltz@gmail.com>
2017-09-02 19:52:14 -0400

    Refactoring simple sections so they can be looped

--- src/App.vue
+++ src/App.vue
@@ -20,78 +20,82 @@ export default {
 	name: 'app',
 	data () {
 		return {
-			armorSelectorArgs: {
-				columns: [
-					{ name: "Armor", key: "name" },
-					{ name: "AC Bonus", key: "acBonus", center: true },
-					{ name: "TL Mod", key: "tlMod", hideIfZero: true, center: true },
-					{ name: "Turn Distance", key: "turnDistance", hideIfZero: true, center: true },
-					{ name: "Cost", key: "cost", multiplyBySize: true, addendum: "based on size", center: true },
-				],
-				items: armors,
-				selectTitle: "Select armor",
-				selectAction: SET_ARMOR,
-				shipComponentKey: "armor",
-				title: "Armor",
-			},
-			driftEngineSelectorArgs: {
-				columns: [
-					{ name: "Drift engine", key: "name" },
-					{ name: "Rating",       key: "rating", hideIfZero: true, center: true },
-					{ name: "Minimum PCU",  key: "minPcu", hideIfZero: true, center: true },
-					{ name: "Cost",         key: "cost", center: true },
-				],
-				itemDisabled: function (currentShip, driftEngine) {
-					let pcu = currentShip.powerCore.pcu;
-					let minPcu = driftEngine.minPcu;
+			simpleSections: [
+				{ // Power core
+					columns: [
+						{ name: "Power core", key: "name" },
+						{ name: "PCU",        key: "pcu", center: true },
+						{ name: "Cost",       key: "cost", center: true },
+					],
+					itemFilter: function (currentShip, powerCore) {
+						let size = currentShip.frame.size;
+						return powerCore.sizes[size];
+					},
+					items: powerCores,
+					selectTitle: "Select a power core",
+					selectAction: SET_POWER_CORE,
+					shipComponentKey: "powerCore",
+					title: "Power Core",
+				},
+				{ // Thrusters
+					columns: [
+						{ name: "Thrusters",    key: "name" },
+						{ name: "Speed",        key: "speed", center: true },
+						{ name: "Piloting Mod", key: "pilotingMod", center: true },
+						{ name: "PCU Cost",     key: "pcuCost", center: true },
+						{ name: "Cost",         key: "cost", center: true },
+					],
+					itemFilter: function (currentShip, thruster) {
+						let size = currentShip.frame.size;
+						return thruster.size === size;
+					},
+					items: thrusters,
+					selectTitle: "Select thrusters",
+					selectAction: SET_THRUSTERS,
+					shipComponentKey: "thrusters",
+					title: "Thrusters",
+				},
+				{ // Drift engine
+					columns: [
+						{ name: "Drift engine", key: "name" },
+						{ name: "Rating",       key: "rating", hideIfZero: true, center: true },
+						{ name: "Minimum PCU",  key: "minPcu", hideIfZero: true, center: true },
+						{ name: "Cost",         key: "cost", center: true },
+					],
+					itemDisabled: function (currentShip, driftEngine) {
+						let pcu = currentShip.powerCore.pcu;
+						let minPcu = driftEngine.minPcu;
 
-					return pcu >= minPcu;
+						console.log("Disabling", pcu, minPcu);
+
+						return pcu >= minPcu;
+					},
+					itemFilter: function (currentShip, driftEngine) {
+						let maxSize = sizeToInt[driftEngine.maxSize];
+						let size = sizeToInt[currentShip.frame.size];
+						return size <= maxSize;
+					},
+					items: driftEngines,
+					selectTitle: "Select a drift engine",
+					selectAction: SET_DRIFT_ENGINE,
+					shipComponentKey: "driftEngine",
+					title: "Drift Engine",
 				},
-				itemFilter: function (currentShip, driftEngine) {
-					let maxSize = sizeToInt[driftEngine.maxSize];
-					let size = sizeToInt[currentShip.frame.size];
-					return size <= maxSize;
+				{ // Armor
+					columns: [
+						{ name: "Armor", key: "name" },
+						{ name: "AC Bonus", key: "acBonus", center: true },
+						{ name: "TL Mod", key: "tlMod", hideIfZero: true, center: true },
+						{ name: "Turn Distance", key: "turnDistance", hideIfZero: true, center: true },
+						{ name: "Cost", key: "cost", multiplyBySize: true, addendum: "based on size", center: true },
+					],
+					items: armors,
+					selectTitle: "Select armor",
+					selectAction: SET_ARMOR,
+					shipComponentKey: "armor",
+					title: "Armor",
 				},
-				items: driftEngines,
-				selectTitle: "Select a drift engine",
-				selectAction: SET_DRIFT_ENGINE,
-				shipComponentKey: "driftEngine",
-				title: "Drift Engine",
-			},
-			powerCoreSelectorArgs: {
-				columns: [
-					{ name: "Power core", key: "name" },
-					{ name: "PCU",        key: "pcu", center: true },
-					{ name: "Cost",       key: "cost", center: true },
-				],
-				itemFilter: function (currentShip, powerCore) {
-					let size = currentShip.frame.size;
-					return powerCore.sizes[size];
-				},
-				items: powerCores,
-				selectTitle: "Select a power core",
-				selectAction: SET_POWER_CORE,
-				shipComponentKey: "powerCore",
-				title: "Power Core",
-			},
-			thrusterSelectorArgs: {
-				columns: [
-					{ name: "Thrusters",    key: "name" },
-					{ name: "Speed",        key: "speed", center: true },
-					{ name: "Piloting Mod", key: "pilotingMod", center: true },
-					{ name: "PCU Cost",     key: "pcuCost", center: true },
-					{ name: "Cost",         key: "cost", center: true },
-				],
-				itemFilter: function (currentShip, thruster) {
-					let size = currentShip.frame.size;
-					return thruster.size === size;
-				},
-				items: thrusters,
-				selectTitle: "Select thrusters",
-				selectAction: SET_THRUSTERS,
-				shipComponentKey: "thrusters",
-				title: "Thrusters",
-			},
+			],
 		}
 	},
 	components: {
@@ -111,55 +115,21 @@ export default {
 			<select-frame></select-frame>
 		</div>
 
-		<div class="app--section">
+		<div
+			v-for="section in simpleSections"
+			class="app--section"
+		>
 			<single-selector
-				:columns="powerCoreSelectorArgs.columns"
-				:item-filter="powerCoreSelectorArgs.itemFilter"
-				:items="powerCoreSelectorArgs.items"
-				:select-action="powerCoreSelectorArgs.selectAction"
-				:select-title="powerCoreSelectorArgs.selectTitle"
-				:ship-component-key="powerCoreSelectorArgs.shipComponentKey"
-				:title="powerCoreSelectorArgs.title"
+				:columns="section.columns"
+				:item-disabled="section.itemDisabled"
+				:item-filter="section.itemFilter"
+				:items="section.items"
+				:select-action="section.selectAction"
+				:select-title="section.selectTitle"
+				:ship-component-key="section.shipComponentKey"
+				:title="section.title"
 			></single-selector>
 		</div>
-
-		<div class="app--section">
-			<single-selector
-				:columns="thrusterSelectorArgs.columns"
-				:item-filter="thrusterSelectorArgs.itemFilter"
-				:items="thrusterSelectorArgs.items"
-				:select-action="thrusterSelectorArgs.selectAction"
-				:select-title="thrusterSelectorArgs.selectTitle"
-				:ship-component-key="thrusterSelectorArgs.shipComponentKey"
-				:title="thrusterSelectorArgs.title"
-			></single-selector>
-		</div>
-
-		<div class="app--section">
-			<single-selector
-				:columns="driftEngineSelectorArgs.columns"
-				:item-disabled="driftEngineSelectorArgs.itemDisabled"
-				:item-filter="driftEngineSelectorArgs.itemFilter"
-				:items="driftEngineSelectorArgs.items"
-				:select-action="driftEngineSelectorArgs.selectAction"
-				:select-title="driftEngineSelectorArgs.selectTitle"
-				:ship-component-key="driftEngineSelectorArgs.shipComponentKey"
-				:title="driftEngineSelectorArgs.title"
-			></single-selector>
-		</div>
-
-		<div class="app--section">
-			<single-selector
-				:columns="armorSelectorArgs.columns"
-				:item-filter="armorSelectorArgs.itemFilter"
-				:items="armorSelectorArgs.items"
-				:select-action="armorSelectorArgs.selectAction"
-				:select-title="armorSelectorArgs.selectTitle"
-				:ship-component-key="armorSelectorArgs.shipComponentKey"
-				:title="armorSelectorArgs.title"
-			></single-selector>
-		</div>
-
 	</div>
 </template>
 
