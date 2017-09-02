7ec13cc1f0ec39b36d05fc56c71866a6aa7a7318
asmor <itoltz@gmail.com>
2017-09-02 19:57:53 -0400

    Drift engine cost should be based on size

--- src/App.vue
+++ src/App.vue
@@ -16,85 +16,90 @@ import thrusters from "./data/thrusters.json";
 import selectFrame from "./components/select-frame.vue";
 import singleSelector from "./components/single-selector.vue";
 
+let armorSection = {
+	columns: [
+		{ name: "Armor", key: "name" },
+		{ name: "AC Bonus", key: "acBonus", center: true },
+		{ name: "TL Mod", key: "tlMod", hideIfZero: true, center: true },
+		{ name: "Turn Distance", key: "turnDistance", hideIfZero: true, center: true },
+		{ name: "Cost", key: "cost", multiplyBySize: true, addendum: "based on size", center: true },
+	],
+	items: armors,
+	selectTitle: "Select armor",
+	selectAction: SET_ARMOR,
+	shipComponentKey: "armor",
+	title: "Armor",
+};
+let driftEngineSection = {
+	columns: [
+		{ name: "Drift engine", key: "name" },
+		{ name: "Rating",       key: "rating", hideIfZero: true, center: true },
+		{ name: "Minimum PCU",  key: "minPcu", hideIfZero: true, center: true },
+		{ name: "Cost",         key: "cost", multiplyBySize: true, addendum: "based on size", center: true },
+	],
+	itemDisabled: function (currentShip, driftEngine) {
+		let pcu = currentShip.powerCore.pcu || 0;
+		let minPcu = driftEngine.minPcu;
+
+		console.log("Disabling", pcu, minPcu);
+
+		return pcu >= minPcu;
+	},
+	itemFilter: function (currentShip, driftEngine) {
+		let maxSize = sizeToInt[driftEngine.maxSize];
+		let size = sizeToInt[currentShip.frame.size];
+		return size <= maxSize;
+	},
+	items: driftEngines,
+	selectTitle: "Select a drift engine",
+	selectAction: SET_DRIFT_ENGINE,
+	shipComponentKey: "driftEngine",
+	title: "Drift Engine",
+};
+let powerCoreSection = {
+	columns: [
+		{ name: "Power core", key: "name" },
+		{ name: "PCU",        key: "pcu", center: true },
+		{ name: "Cost",       key: "cost", center: true },
+	],
+	itemFilter: function (currentShip, powerCore) {
+		let size = currentShip.frame.size;
+		return powerCore.sizes[size];
+	},
+	items: powerCores,
+	selectTitle: "Select a power core",
+	selectAction: SET_POWER_CORE,
+	shipComponentKey: "powerCore",
+	title: "Power Core",
+};
+let thrustersSection = {
+	columns: [
+		{ name: "Thrusters",    key: "name" },
+		{ name: "Speed",        key: "speed", center: true },
+		{ name: "Piloting Mod", key: "pilotingMod", center: true },
+		{ name: "PCU Cost",     key: "pcuCost", center: true },
+		{ name: "Cost",         key: "cost", center: true },
+	],
+	itemFilter: function (currentShip, thruster) {
+		let size = currentShip.frame.size;
+		return thruster.size === size;
+	},
+	items: thrusters,
+	selectTitle: "Select thrusters",
+	selectAction: SET_THRUSTERS,
+	shipComponentKey: "thrusters",
+	title: "Thrusters",
+};
+
 export default {
 	name: 'app',
 	data () {
 		return {
 			simpleSections: [
-				{ // Power core
-					columns: [
-						{ name: "Power core", key: "name" },
-						{ name: "PCU",        key: "pcu", center: true },
-						{ name: "Cost",       key: "cost", center: true },
-					],
-					itemFilter: function (currentShip, powerCore) {
-						let size = currentShip.frame.size;
-						return powerCore.sizes[size];
-					},
-					items: powerCores,
-					selectTitle: "Select a power core",
-					selectAction: SET_POWER_CORE,
-					shipComponentKey: "powerCore",
-					title: "Power Core",
-				},
-				{ // Thrusters
-					columns: [
-						{ name: "Thrusters",    key: "name" },
-						{ name: "Speed",        key: "speed", center: true },
-						{ name: "Piloting Mod", key: "pilotingMod", center: true },
-						{ name: "PCU Cost",     key: "pcuCost", center: true },
-						{ name: "Cost",         key: "cost", center: true },
-					],
-					itemFilter: function (currentShip, thruster) {
-						let size = currentShip.frame.size;
-						return thruster.size === size;
-					},
-					items: thrusters,
-					selectTitle: "Select thrusters",
-					selectAction: SET_THRUSTERS,
-					shipComponentKey: "thrusters",
-					title: "Thrusters",
-				},
-				{ // Drift engine
-					columns: [
-						{ name: "Drift engine", key: "name" },
-						{ name: "Rating",       key: "rating", hideIfZero: true, center: true },
-						{ name: "Minimum PCU",  key: "minPcu", hideIfZero: true, center: true },
-						{ name: "Cost",         key: "cost", center: true },
-					],
-					itemDisabled: function (currentShip, driftEngine) {
-						let pcu = currentShip.powerCore.pcu || 0;
-						let minPcu = driftEngine.minPcu;
-
-						console.log("Disabling", pcu, minPcu);
-
-						return pcu >= minPcu;
-					},
-					itemFilter: function (currentShip, driftEngine) {
-						let maxSize = sizeToInt[driftEngine.maxSize];
-						let size = sizeToInt[currentShip.frame.size];
-						return size <= maxSize;
-					},
-					items: driftEngines,
-					selectTitle: "Select a drift engine",
-					selectAction: SET_DRIFT_ENGINE,
-					shipComponentKey: "driftEngine",
-					title: "Drift Engine",
-				},
-				{ // Armor
-					columns: [
-						{ name: "Armor", key: "name" },
-						{ name: "AC Bonus", key: "acBonus", center: true },
-						{ name: "TL Mod", key: "tlMod", hideIfZero: true, center: true },
-						{ name: "Turn Distance", key: "turnDistance", hideIfZero: true, center: true },
-						{ name: "Cost", key: "cost", multiplyBySize: true, addendum: "based on size", center: true },
-					],
-					items: armors,
-					selectTitle: "Select armor",
-					selectAction: SET_ARMOR,
-					shipComponentKey: "armor",
-					title: "Armor",
-				},
+				powerCoreSection,
+				thrustersSection,
+				driftEngineSection,
+				armorSection,
 			],
 		}
 	},
