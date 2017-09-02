6cbad15fc1d4ee34ec9579a2370b058e61a8c05a
asmor <itoltz@gmail.com>
2017-09-02 13:50:46 -0400

    Simplified store configuration

--- src/store.js
+++ src/store.js
@@ -6,16 +6,7 @@ import thrusters from "./data/thrusters.json";
 
 Vue.use(Vuex);
 
-const SET_ARMOR = "SET_ARMOR";
-const SET_ARMOR_MUTATION = "SET_ARMOR_MUTATION";
-const SET_FRAME = "SET_FRAME";
-const SET_FRAME_MUTATION = "SET_FRAME_MUTATION";
-const SET_POWER_CORE = "SET_POWER_CORE";
-const SET_POWER_CORE_MUTATION = "SET_POWER_CORE_MUTATION";
-const SET_THRUSTERS = "SET_THRUSTERS";
-const SET_THRUSTERS_MUTATION = "SET_THRUSTERS_MUTATION";
-
-const store = new Vuex.Store({
+let config = {
 	strict: true,
 	state: {
 		currentShip: {
@@ -25,50 +16,56 @@ const store = new Vuex.Store({
 			thrusters: false,
 		},
 	},
-	actions: {
-		SET_ARMOR: ({ commit }, frame) => {
-			commit(SET_ARMOR_MUTATION, frame);
-		},
-		SET_FRAME: ({ commit }, frame) => {
-			commit(SET_FRAME_MUTATION, frame);
-		},
-		SET_POWER_CORE: ({ commit }, powerCore) => {
-			commit(SET_POWER_CORE_MUTATION, powerCore);
-		},
-		SET_THRUSTERS: ({ commit }, thruster) => {
-			commit(SET_THRUSTERS_MUTATION, thruster);
-		},
-	},
-	mutations: {
-		SET_ARMOR_MUTATION: (state, armor) => {
-			state.currentShip.armor = armor;
-		},
-		SET_FRAME_MUTATION: (state, frame) => {
-			state.currentShip.frame = frame;
-
-			// Check if the power core is valid for the new frame size and, if not, switch to the
-			// first power core that is
-			var powerCoreValid = state.currentShip.powerCore && state.currentShip.powerCore.sizes[frame.size];
-			if ( !powerCoreValid ) {
-				state.currentShip.powerCore = powerCores.find(core => core.sizes[frame.size]);
-			}
-
-			// Check if the power core is valid for the new frame size and, if not, switch to the
-			// first power core that is
-			var thrustersValid = state.currentShip.thrusters && (state.currentShip.thrusters.size === frame.size);
-			if ( !thrustersValid ) {
-				state.currentShip.thrusters = thrusters.find(thruster => thruster.size === frame.size);
-			}
-		},
-		SET_POWER_CORE_MUTATION: (state, powerCore) => {
-			state.currentShip.powerCore = powerCore;
-		},
-		SET_THRUSTERS_MUTATION: (state, thruster) => {
-			state.currentShip.thrusters = thruster;
-		},
-	},
+	actions: {},
+	mutations: {},
 	getters: {},
+};
+
+const SET_ARMOR = "SET_ARMOR";
+const SET_ARMOR_MUTATION = "SET_ARMOR_MUTATION";
+const SET_FRAME = "SET_FRAME";
+const SET_FRAME_MUTATION = "SET_FRAME_MUTATION";
+const SET_POWER_CORE = "SET_POWER_CORE";
+const SET_POWER_CORE_MUTATION = "SET_POWER_CORE_MUTATION";
+const SET_THRUSTERS = "SET_THRUSTERS";
+const SET_THRUSTERS_MUTATION = "SET_THRUSTERS_MUTATION";
+
+// Setup everything with simple default actions and mutations, override with more complex logic as
+// needed
+[
+	{ action: SET_ARMOR,      mutation: SET_ARMOR_MUTATION,      shipComponent: "armor" },
+	{ action: SET_FRAME,      mutation: SET_FRAME_MUTATION,      shipComponent: "frame" },
+	{ action: SET_POWER_CORE, mutation: SET_POWER_CORE_MUTATION, shipComponent: "powerCore" },
+	{ action: SET_THRUSTERS,  mutation: SET_THRUSTERS_MUTATION,  shipComponent: "thrusters" },
+].forEach(function (args) {
+	config.actions[args.action] = ({ commit }, item) => {
+		commit(args.mutation, item);
+	};
+
+	config.mutations[args.mutation] = (state, item) => {
+		state.currentShip[args.shipComponent] = item;
+	};
 });
+
+config.mutations[SET_FRAME_MUTATION] = (state, frame) => {
+	state.currentShip.frame = frame;
+
+	// Check if the power core is valid for the new frame size and, if not, switch to the
+	// first power core that is
+	var powerCoreValid = state.currentShip.powerCore && state.currentShip.powerCore.sizes[frame.size];
+	if ( !powerCoreValid ) {
+		state.currentShip.powerCore = powerCores.find(core => core.sizes[frame.size]);
+	}
+
+	// Check if the power core is valid for the new frame size and, if not, switch to the
+	// first power core that is
+	var thrustersValid = state.currentShip.thrusters && (state.currentShip.thrusters.size === frame.size);
+	if ( !thrustersValid ) {
+		state.currentShip.thrusters = thrusters.find(thruster => thruster.size === frame.size);
+	}
+};
+
+const store = new Vuex.Store(config);
 
 export {
 	store,
