09170252783ac6d3a477f6b01bae3a90420d6686
asmor <itoltz@gmail.com>
2017-09-07 02:50:10 -0400

    Expansion bays partially working

---
+++ src/components/expansion-bay-select-modal.vue
@@ -0,0 +1,150 @@
+<script>
+import {
+	store,
+	ADD_EXPANSION_BAY,
+} from "../store.js";
+import expansionBays from "../data/expansion-bays.json";
+import {
+	nameSort,
+} from "../util.js";
+
+import {
+	registerModal, 
+	EXPANSION_BAY_SELECT_MODAL, 
+} from "../modal-handler.js";
+
+export default {
+	name: "expansionBaySelectModal",
+	store,
+	data () {
+		return {
+			expansionBays,
+			modalId: "expansion-bay-select-modal",
+		};
+	},
+	computed: {},
+	mounted: function () {
+		registerModal({
+			modalId: EXPANSION_BAY_SELECT_MODAL,
+			opener: () => {
+				this.$refs[this.modalId].show();
+			},
+		});
+	},
+	methods: {
+		addExpansionBay(expansionBay) {
+			if ( this.expansionBayAllowed(expansionBay) ) {
+				this.$store.dispatch(ADD_EXPANSION_BAY, expansionBay);
+				this.$refs[this.modalId].hide();
+			}
+		},
+		expansionBayAllowed(expansionBay) {
+			// TODO: Check that we have enough slots for the expansion bay and the ship's frame is
+			// big enough to allow it
+			return true;
+		},
+	},
+	components: {},
+};
+</script>
+
+<template>
+	<div class="expansion-bay-select">
+		<b-modal
+			:id="modalId"
+			class="expansion-bay-select-modal"
+			:ref="modalId"
+			title="Choose expansion bay"
+		>
+			<table class="expansion-bay-select-modal--table">
+				<thead>
+					<tr class="expansion-bay-select-modal--header">
+						<td>
+							Name
+						</td>
+						<td>
+							Min. Ship Size
+						</td>
+						<td class="expansion-bay-select-modal--column__centered">
+							Bays
+						</td>
+						<td class="expansion-bay-select-modal--column__centered">
+							PCU
+						</td>
+						<td class="expansion-bay-select-modal--column__centered">
+							Cost
+						</td>
+					</tr>
+				</thead>
+				<tbody>
+					<tr
+						v-for="expansionBay in expansionBays"
+						class="expansion-bay-select-modal--body"
+						:class="{ 'expansion-bay-select-modal--body__disabled': !expansionBayAllowed(expansionBay) }"
+						@click="addExpansionBay(expansionBay)"
+					>
+						<td>
+							{{ expansionBay.name }}
+						</td>
+						<td>
+							{{ expansionBay.minSize }}
+						</td>
+						<td class="expansion-bay-select-modal--column__centered">
+							{{ expansionBay.bays }}
+						</td>
+						<td class="expansion-bay-select-modal--column__centered">
+							{{ expansionBay.pcu }}
+						</td>
+						<td class="expansion-bay-select-modal--column__centered">
+							{{ expansionBay.cost }}
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<div slot="modal-footer"></div>
+		</b-modal>
+	</div>
+</template>
+
+<style lang="scss">
+.expansion-bay-select-modal {
+	.expansion-bay-select-modal--table {
+		width: 100%;
+	}
+
+	.expansion-bay-select-modal--header,
+	.expansion-bay-select-modal--body {
+		& > td {
+			padding: 5px;
+		}
+	}
+
+	.expansion-bay-select-modal--header {
+		font-weight: bold;
+		background-color: #000;
+		color: #fff;
+	}
+
+	.expansion-bay-select-modal--body {
+		cursor: pointer;
+
+		&:hover {
+			background-color: #ddd;
+		}
+
+		&.expansion-bay-select-modal--body__selected {
+			background-color: #dfd;
+		}
+
+		&.expansion-bay-select-modal--body__disabled {
+			background-color: inherit;
+			cursor: not-allowed;
+			color: #ccc;
+		}
+	}
+
+	.expansion-bay-select-modal--column__centered {
+		text-align: center;
+	}
+}
+</style>
---
+++ src/components/expansions-bays.vue
@@ -0,0 +1,93 @@
+<script>
+import { store } from "../store.js";
+
+import {
+	openModal,
+	EXPANSION_BAY_OPTIONS_MODAL,
+	EXPANSION_BAY_SELECT_MODAL,
+} from "../modal-handler.js";
+
+import singleItem from "./single-item.vue";
+
+export default {
+	name: "expansionBays",
+	store,
+	data: function () {
+		return {};
+	},
+	computed: {
+		maxSlots: function () {
+			return this.$store.state.currentShip.frame.expansionBays;
+		},
+		remainingSlots: function () {
+			return this.maxSlots - this.usedSlots;
+		},
+		selectedExpansionBays: function () {
+			return this.$store.state.currentShip.expansionBays;
+		},
+		usedSlots: function () {
+			return this.selectedExpansionBays.reduce((sum, bay) => { return sum + bay.bays; }, 0);
+		},
+	},
+	methods: {
+		getExpansionBayText(expansionBay) {
+			let parts = [];
+			let { bays, pcu, cost } = expansionBay;
+
+			if ( bays > 1 ) {
+				parts.push(bays + " bays");
+			}
+
+			parts.push("PCU " + pcu);
+			parts.push("Cost " + cost);
+
+			return parts.join("; ");
+		},
+		showExpansionBaySelectModal() {
+			openModal({ modalId: EXPANSION_BAY_SELECT_MODAL });
+		},
+	},
+	components: {
+		singleItem,
+	},
+}
+</script>
+
+<template>
+	<div class="expansion-bays">
+		<single-item
+			title="Expansion bays"
+			no-rollover="true"
+		>
+			Slots available: {{ remainingSlots }} / {{ maxSlots }}
+
+			<div
+				class="expansion-bays--expansionBay"
+				v-for="expansionBay in selectedExpansionBays"
+			>
+				<!-- @click="showexpansionBayOptionsModal(expansionBay)" -->
+				<span class="expansion-bays--expansion-bay-title">{{ expansionBay.name }}</span>
+				<span class="expansion-bays--expansion-bay-description">{{ getExpansionBayText(expansionBay) }}</span>
+			</div>
+
+			<b-button
+				variant="primary"
+				@click="showExpansionBaySelectModal()"
+				:disabled="remainingSlots <= 0"
+			>Add expansion bay</b-button>
+		</single-item>
+	</div>
+</template>
+
+<style lang="scss">
+.expansion-bays {
+	.expansion-bays--expansion-bay-title {
+		font-weight: bold;
+	}
+	
+	.expansion-bays--expansion-bay-description {
+		display: block;
+		padding-left: 10px;
+	}
+}
+</style>
